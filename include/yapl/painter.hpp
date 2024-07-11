eb5cc277ecad50cb1bc24dd482bdd1e89ec09be1
Szymon Wąchała <wachim17@gmail.com>
2024-07-11 20:20:45 +0200

    introduced interpolation, need to fix it also :D

--- include/yapl/painter.hpp
+++ include/yapl/painter.hpp
@@ -3,5 +3,20 @@
 #include "yapl.hpp"
 
 namespace yapl {
+    /// @brief Function to interpolate data from given X and Y on to desired pixels vectors
+    /// @param x_from 
+    /// @param y_from 
+    /// @param x_to 
+    /// @param y_to 
+    void interpolate_data_over_pixels(const std::vector<double>& x_from, 
+        const std::vector<double>& y_from, 
+        const std::vector<uint16_t>& x_to, 
+        std::vector<uint16_t>& y_to);
+        
+    /// @brief Function responsible for drawing plot 
+    /// @param plot Plot object
+    /// @param path path to save plot 
+    /// @param width pixel number for width
+    /// @param height pixel number for height
     void drawPlot(const Plot& plot, const std::filesystem::path& path, const uint16_t width = 800, const uint16_t height = 600);
 }
--- src/painter.cpp
+++ src/painter.cpp
@@ -4,6 +4,50 @@
 #include <stdexcept>
 
 namespace yapl {
+    void interpolate_data_over_pixels(const std::vector<double>& x_from, 
+        const std::vector<double>& y_from, 
+        const std::vector<uint16_t>& x_to, 
+        std::vector<uint16_t>& y_to) {
+        // code for implementation
+        if (x_from.size() != y_from.size()) {
+            throw Exception("x_from and y_from must have the same size");
+        }
+
+        y_to.clear();
+        y_to.reserve(x_to.size());
+
+        for (const auto& x : x_to) {
+            // Find the interval [x_from[i], x_from[i+1]] that contains x
+            auto it = std::lower_bound(x_from.begin(), x_from.end(), x);
+            
+            if (it == x_from.end()) {
+                // If x is beyond the last element of x_from, use the last interval
+                it = x_from.end() - 1;
+            } 
+            else if (it != x_from.begin() && *it != x) {
+                // If x is not exactly equal to an element in x_from, use the previous interval
+                --it;
+            }
+
+            size_t i = std::distance(x_from.begin(), it);
+            
+            // Linear interpolation
+            if (i + 1 < x_from.size()) {
+                double x0 = x_from[i];
+                double y0 = y_from[i];
+                double x1 = x_from[i + 1];
+                double y1 = y_from[i + 1];
+
+                double y = y0 + (x - x0) * (y1 - y0) / (x1 - x0);
+                y_to.push_back(static_cast<uint16_t>(y));
+            } 
+            else {
+                // If we are at the last element, just use the last y value
+                y_to.push_back(static_cast<uint16_t>(y_from[i]));
+            }
+        }
+    }
+    
     void drawPlot(const Plot& plot, const std::filesystem::path& path, const uint16_t width, const uint16_t height) {
         cairo_surface_t* surface = cairo_image_surface_create(CAIRO_FORMAT_ARGB32, width, height);
         cairo_t* cr = cairo_create(surface);
@@ -30,9 +74,19 @@ namespace yapl {
         cairo_set_line_width(cr, 1.5);
         for (size_t i = 0; i < plot._x.size(); ++i) {
             cairo_set_source_rgb(cr, 0, 0, 1); // Blue color for lines
-            cairo_move_to(cr, 50 + plot._x[i][0], height - 50 - plot._y[i][0]);
-            for (size_t j = 1; j < plot._x[i].size(); ++j) {
-                cairo_line_to(cr, 50 + plot._x[i][j], height - 50 - plot._y[i][j]);
+            
+            // Interpolacja danych
+            std::vector<uint16_t> x_pixels;
+            std::vector<uint16_t> y_pixels;
+            for (double x_val : plot._x[i]) {
+                x_pixels.push_back(static_cast<uint16_t>(50 + x_val));
+            }
+            interpolate_data_over_pixels(plot._x[i], plot._y[i], x_pixels, y_pixels);
+
+            // Rysowanie interpolowanych danych
+            cairo_move_to(cr, x_pixels[0], height - 50 - y_pixels[0]);
+            for (size_t j = 1; j < x_pixels.size(); ++j) {
+                cairo_line_to(cr, x_pixels[j], height - 50 - y_pixels[j]);
             }
             cairo_stroke(cr);
         }
